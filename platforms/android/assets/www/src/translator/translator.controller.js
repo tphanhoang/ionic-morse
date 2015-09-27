1dd25880f0649fd5758497a90fe3089299206717
tphanhoang <phan.thibaut@gmail.com>
2015-09-27 19:17:59 +0200

    flashlight OK

--- platforms/android/assets/www/src/translator/translator.controller.js
+++ platforms/android/assets/www/src/translator/translator.controller.js
@@ -31,39 +31,61 @@ angular.module('morse')
 		var i = 0;
 		var trait = 0;		
 		var flash_actived = false;
+		var space = 0;
 
 		var runner = setInterval(function(){			
 			if(i<msgArray.length){				
 				if(msgArray[i]=="750"){
 					if(!flash_actived){
 						window.plugins.flashlight.switchOn();
+						console.log("750 ON");	
 						trait = trait + 1;
 						flash_actived = true;
 					}
 					else if(flash_actived && trait == 2){
+						console.log("750 Last");	
 						trait = 0;
 						i=i+1;
 					}else{
+						console.log("750 Middle");	
 						trait = trait + 1;
 					}
 				}
 				else if(msgArray[i]=="250"){
 					if(!flash_actived){
+						console.log("250 ON");	
 						window.plugins.flashlight.switchOn();
 						flash_actived = true;
 						i=i+1;
 					}else{
+						console.log("250 OFF");	
 						window.plugins.flashlight.switchOff();
+						space = space+1;
 						flash_actived = false;
 						i=i+1;
 					}
 				}
-				console.log(msgArray);				
+				else if(msgArray[i]=="1750"){
+					if(flash_actived){
+						console.log("1750 OFF");	
+						window.plugins.flashlight.switchOff();
+						flash_actived = false;
+					}
+					else if(!flash_actived && space == 6){
+						console.log("1750 Last");	
+						space = 0;
+						i=i+1;
+					}else{
+						console.log("750 Middle");	
+						space = space+1;
+					}
+				}
+							
 			}else{
 				clearInterval(runner)
 			}
 
-		}, 100);
+		}, 500);
 
 }
 });
--- www/src/translator/translator.controller.js
+++ www/src/translator/translator.controller.js
@@ -8,7 +8,7 @@ angular.module('morse')
     vm.translate = function (formatOutput, morseInput) {    	
     	formatOutput['format'] = formatOutput[0];  
         vm.morseOutput = Translator.translate(formatOutput,morseInput)
-        console.log(vm.morseOutput);
+        // console.log(vm.morseOutput);
     };
 
 
@@ -16,9 +16,8 @@ angular.module('morse')
     vm.vibrationTranslate = function(morseInput) {
     	formatOutput = [];
     	formatOutput['format'] = 'time';    	
-        vm.morseOutput = Translator.translate(formatOutput,morseInput);
-        setT
-        console.log(vm.morseOutput);
+        vm.morseOutput = Translator.translate(formatOutput,morseInput);        
+        // console.log(vm.morseOutput);
     };
 
     vm.flashLightTranslate = function(morseInput) {
@@ -31,36 +30,53 @@ angular.module('morse')
 		var i = 0;
 		var trait = 0;		
 		var flash_actived = false;
+		var space = 0;
 
 		var runner = setInterval(function(){			
 			if(i<msgArray.length){				
 				if(msgArray[i]=="750"){
 					if(!flash_actived){
-						// window.plugins.flashlight.switchOn();
-						console.log("750 ON");	
+						window.plugins.flashlight.switchOn();
+						// console.log("750 ON");	
 						trait = trait + 1;
 						flash_actived = true;
 					}
 					else if(flash_actived && trait == 2){
-						console.log("750 Last");	
+						// console.log("750 Last");	
 						trait = 0;
 						i=i+1;
 					}else{
-						console.log("750 Middle");	
+						// console.log("750 Middle "+trait);	
 						trait = trait + 1;
 					}
 				}
 				else if(msgArray[i]=="250"){
 					if(!flash_actived){
-						console.log("250 ON");	
-						// window.plugins.flashlight.switchOn();
+						// console.log("250 ON");	
+						window.plugins.flashlight.switchOn();
 						flash_actived = true;
 						i=i+1;
 					}else{
-						console.log("250 OFF");	
-						// window.plugins.flashlight.switchOff();
+						// console.log("250 OFF");	
+						window.plugins.flashlight.switchOff();
+						space = space+1;
 						flash_actived = false;
 						i=i+1;
+					}
+				}
+				else if(msgArray[i]=="1750"){
+					if(flash_actived){
+						// console.log("1750 OFF");	
+						window.plugins.flashlight.switchOff();
+						flash_actived = false;
+					}
+					else if(!flash_actived && space == 6){
+						// console.log("1750 Last");	
+						space = 0;
+						i=i+1;
+					}else{
+						// console.log("750 Middle");	
+						space = space+1;
 					}
 				}
 							
@@ -68,7 +84,7 @@ angular.module('morse')
 				clearInterval(runner)
 			}
 
-		}, 250);
+		}, 500);
 
 }
 });
